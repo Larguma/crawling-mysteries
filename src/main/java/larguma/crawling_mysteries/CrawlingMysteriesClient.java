3660cd2f9bdb57169632a3fe811e2e931ee051b2
Larguma <larguma@hotmail.fr>
2024-02-20 20:39:03 +0100

    [feat] Added options to hide cryptic_eye

--- src/main/java/larguma/crawling_mysteries/CrawlingMysteriesClient.java
+++ src/main/java/larguma/crawling_mysteries/CrawlingMysteriesClient.java
@@ -15,22 +15,23 @@ public class CrawlingMysteriesClient implements ClientModInitializer {
   @SuppressWarnings("unchecked")
   @Override
   public void onInitializeClient() {
-    TrinketRendererRegistry.registerRenderer(ModItems.CRYPTIC_EYE,
-        (stack, slotReference, contextModel, matrices, vertexConsumers, light, entity, limbAngle, limbDistance,
-            tickDelta, animationProgress, headYaw, headPitch) -> {
-          if (entity instanceof AbstractClientPlayerEntity player) {
-            TrinketRenderer.translateToFace(matrices,
-                (PlayerEntityModel<AbstractClientPlayerEntity>) contextModel, player, headYaw, headPitch);
-            // matrices.scale(x, y, z);
-            // x: + gauche; - droite
-            // y: + bas; - haut
-            // z: + derrière; - devant
-            matrices.translate(-0.2F, -0.4F, 0.5F);
-            MinecraftClient.getInstance().getItemRenderer()
-                .renderItem(stack, ModelTransformationMode.HEAD, light, OverlayTexture.DEFAULT_UV, matrices,
-                    vertexConsumers, null, 0);
-          }
-        });
+    if (CrawlingMysteries.CONFIG.enableCrypticEyeRender())
+      TrinketRendererRegistry.registerRenderer(ModItems.CRYPTIC_EYE,
+          (stack, slotReference, contextModel, matrices, vertexConsumers, light, entity, limbAngle, limbDistance,
+              tickDelta, animationProgress, headYaw, headPitch) -> {
+            if (entity instanceof AbstractClientPlayerEntity player) {
+              TrinketRenderer.translateToFace(matrices,
+                  (PlayerEntityModel<AbstractClientPlayerEntity>) contextModel, player, headYaw, headPitch);
+              // matrices.scale(x, y, z);
+              // x: + gauche; - droite
+              // y: + bas; - haut
+              // z: + derrière; - devant
+              matrices.translate(-0.2F, -0.4F, 0.5F);
+              MinecraftClient.getInstance().getItemRenderer()
+                  .renderItem(stack, ModelTransformationMode.HEAD, light, OverlayTexture.DEFAULT_UV, matrices,
+                      vertexConsumers, null, 0);
+            }
+          });
   }
 
 }
