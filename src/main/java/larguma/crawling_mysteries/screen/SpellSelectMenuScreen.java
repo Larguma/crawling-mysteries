5a1fe661f996fbb5d0851a36b7afc9cb3a8087bb
Larguma <larguma@hotmail.fr>
2024-03-12 15:11:15 +0100

    [feat] updated spell menu

--- src/main/java/larguma/crawling_mysteries/screen/SpellSelectMenuScreen.java
+++ src/main/java/larguma/crawling_mysteries/screen/SpellSelectMenuScreen.java
@@ -1,30 +1,17 @@
 package larguma.crawling_mysteries.screen;
 
 import io.wispforest.owo.ui.base.BaseUIModelScreen;
-import io.wispforest.owo.ui.component.ButtonComponent;
-import io.wispforest.owo.ui.container.GridLayout;
+import io.wispforest.owo.ui.container.FlowLayout;
 import larguma.crawling_mysteries.CrawlingMysteries;
 import net.minecraft.util.Identifier;
 
-public class SpellSelectMenuScreen extends BaseUIModelScreen<GridLayout> {
+public class SpellSelectMenuScreen extends BaseUIModelScreen<FlowLayout> {
 
   public SpellSelectMenuScreen() {
-    super(GridLayout.class, DataSource.asset(new Identifier(CrawlingMysteries.MOD_ID, "spell_menu_screen")));
+    super(FlowLayout.class, DataSource.asset(new Identifier(CrawlingMysteries.MOD_ID, "spell_menu_screen")));
   }
 
   @Override
-  protected void build(GridLayout rootComponent) {
-    rootComponent.childById(ButtonComponent.class, "favorite-spell-one").onPress(button -> {
-      CrawlingMysteries.LOGGER.info("Button favorite spell one pressed!");
-    });
-    rootComponent.childById(ButtonComponent.class, "favorite-spell-two").onPress(button -> {
-      CrawlingMysteries.LOGGER.info("Button favorite spell two pressed!");
-    });
-    rootComponent.childById(ButtonComponent.class, "favorite-spell-three").onPress(button -> {
-      CrawlingMysteries.LOGGER.info("Button favorite spell three pressed!");
-    });
-    rootComponent.childById(ButtonComponent.class, "favorite-spell-four").onPress(button -> {
-      CrawlingMysteries.LOGGER.info("Button favorite spell four pressed!");
-    });
+  protected void build(FlowLayout rootComponent) {
   }
 }
